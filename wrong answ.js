28acfd96228e8e0b99b3943b54afd435ca2dee57
offkoff <offkolianoff@gmail.com>
2017-06-07 09:10:22 +0800

    wrong func

---
+++ wrong answ.js
@@ -0,0 +1,13 @@
+
+// BEGIN (write your solution here)
+export const addDigits = num => {
+  let str = String(num);
+  let result = 0;
+  for (let i = 0; i < strings.length(str); i++) {
+    result += Number(str[i]);
+  if (strings.length(result) >1) {
+    str = String(result);}
+  else return result; 
+  }
+};
+// END
