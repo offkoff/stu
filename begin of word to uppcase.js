87ec34c2d187c008dde1531b22389f331f76634c
offkoff <offkolianoff@gmail.com>
2017-06-08 14:45:42 +0800

    letter to uppercase

--- begin of word to uppcase.js
+++ begin of word to uppcase.js
@@ -2,8 +2,9 @@
 export default (str) => {
   let result = '';
   for (let i = 0; i < strings.length(str); i += 1) {
+      //(первый символ или предидущий сивол пробел) и не пробел 
     const shouldBeBig = str[i] !== ' ' && (i === 0 || str[i - 1] === ' ');
-    result += shouldBeBig ? strings.toUpperCase(str[i]) : str[i]; //?
+    result += shouldBeBig ? strings.toUpperCase(str[i]) : str[i]; // если true - то первое, false - второе
   }
 
   return result;
