9f198f1115b10b208fdf405b0b828271f70d324c
offkoff <offkolianoff@gmail.com>
2017-06-12 18:11:38 +0800

    flip arg

---
+++ pair & point.js
@@ -0,0 +1,9 @@
+// конструктор создает пару
+const cons = (x, y) => (m) => m(x, y);
+
+const car = z => z((x, y) => x);
+const cdr = z => z((x, y) => y);
+
+const point = cons(x, y);
+const x = car(point);
+const y = cdr(point);
